78699426f744a01bc364e778dc8210d3d0e9483f
Bowen Zhang <bowenzhangusa@yahoo.com>
2016-08-29 13:46:07 -0700

    Initial Commit

---
+++ testSegueTests/testSegueTests.swift
@@ -0,0 +1,36 @@
+//
+//  testSegueTests.swift
+//  testSegueTests
+//
+//  Created by Bowen Zhang on 8/29/16.
+//  Copyright © 2016 Bowen Zhang. All rights reserved.
+//
+
+import XCTest
+@testable import testSegue
+
+class testSegueTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
